6e1ea4f54c914d0f6bbca24eb6f747721c349980
Ilya Kozak <kozak@tut.by>
2023-03-28 16:19:41 +0300

    refactor: header component to hooks

--- src/components/Header/HeaderNavigation.tsx
+++ src/components/Header/HeaderNavigation.tsx
@@ -6,27 +6,25 @@ import { pagePath } from '../../constants/constants';
 import getObjKeyFromValue from '../../utils/getObjKeyFromValue';
 import './HeaderNavigation.css';
 
-class HeaderNavigation extends React.Component<WithRouterProps> {
-  render() {
-    const path = this.props.location.pathname;
+const HeaderNavigation: React.FC<WithRouterProps> = (props) => {
+  const path = props.location.pathname;
 
-    return (
-      <header>
-        <h2>{getObjKeyFromValue(pagePath, path)}</h2>
-        <nav>
-          <ul>
-            {(Object.keys(pagePath) as Array<keyof typeof pagePath>).map((page) => {
-              return (
-                <li key={page}>
-                  <NavLink to={pagePath[page]}>{page}</NavLink>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-}
+  return (
+    <header>
+      <h2>{getObjKeyFromValue(pagePath, path)}</h2>
+      <nav>
+        <ul>
+          {(Object.keys(pagePath) as Array<keyof typeof pagePath>).map((page) => {
+            return (
+              <li key={page}>
+                <NavLink to={pagePath[page]}>{page}</NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default withRouter(HeaderNavigation);
